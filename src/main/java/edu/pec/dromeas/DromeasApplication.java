72e77a2e5eed48fa53e006249d5a6236ae773558
Sidharth Bhandari <sample1561>
2020-02-19 16:23:40 +0530

    project structure

---
+++ src/main/java/edu/pec/dromeas/DromeasApplication.java
@@ -0,0 +1,13 @@
+package edu.pec.dromeas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DromeasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DromeasApplication.class, args);
+    }
+
+}
